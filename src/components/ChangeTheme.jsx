dfde88bf312f62ba7dd5ff66d1c3e295f861a429
Rave <148402548+liril-co@users.noreply.github.com>
2025-05-13 09:54:59 -0500

    otro

--- src/components/ChangeTheme.jsx
+++ src/components/ChangeTheme.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {useAppContext} from "../context/AppContext"
 export const ChangeTheme = () => {
     const {theme, handleTheme}= useAppContext()
 
+    useEffect(()=>{
+        document.body.className= theme;
+    }, [theme])
   return( 
     <>
     <button onClick={handleTheme}>Change Theme</button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {AppUseContext} from './AppUseContext.jsx'
+import "./styles/styles.css"
+import { AppProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AppUseContext />
+    <AppProvider>
+      <AppUseContext />
+    </AppProvider>
   </StrictMode>,
 )
